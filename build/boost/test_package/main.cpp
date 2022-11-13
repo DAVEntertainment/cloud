79c0a6832a773087a649ef02d8d6de9867c7b63f
davied <wuwei_543333827@126.com>
2022-11-13 12:05:22 +0800

    update boost test_package

--- build/boost/test_package/main.cpp
+++ build/boost/test_package/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <boost/version.hpp>
-#include <boost/chrono.hpp>
+#include <boost/date_time.hpp>
 
 int main() {
+    auto now = boost::posix_time::second_clock::local_time();
+    std::cout << "Starting at: " << now << std::endl;
     std::cout << "Boost version: " << BOOST_VERSION << std::endl;
     return 0;
 }
