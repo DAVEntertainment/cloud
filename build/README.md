b28ccfa7d918d55009b442affa08089975c69593
Wei.Wu <Wei.Wu@marvelstonemed.com>
2023-01-27 17:26:28 +0800

    remove build scripts from binaries branch

---
+++ build/README.md
@@ -0,0 +1,2 @@
+# Cloud thirdparties build scripts
+This directory is reserved for the branch `build` of Cloud
--- build/gtest/test_package/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include <GTest/gtest.h>
-
-TEST(test_package, 00) {
-    EXPECT_EQ(0, 0);
-}
