b917a2d22975e7cbbc1400dd98d86e87f1785a6e
Wei.Wu <Wei.Wu@marvelstonemed.com>
2023-01-29 10:36:16 +0800

    run test_package in build.cmd

--- build/gflags/build.cmd
+++ build/gflags/build.cmd
@@ -6,31 +6,55 @@
 @set SOURCE_DIR=%SCRIPT_DIR%\gflags
 @set BUILD_DIR=%SOURCE_DIR%\.build
 @set INSTALL_DIR=%SCRIPT_DIR%\..\..\gflags-%VERSION%
+@set TEST_PACKAGE_DIR=%SCRIPT_DIR%\test_package
+@set TEST_PACKAGE_BUILD_DIR=%TEST_PACKAGE_DIR%\.build
 
 @echo ##########################################################################################################
 @echo building gflags
-@echo   SOURCE_DIR   : %SOURCE_DIR%
-@echo   VERSION      : %VERSION%
-@echo   BUILD_DIR    : %BUILD_DIR%
-@echo   INSTALL_DIR  : %INSTALL_DIR%
+@echo   SOURCE_DIR              : %SOURCE_DIR%
+@echo   VERSION                 : %VERSION%
+@echo   BUILD_DIR               : %BUILD_DIR%
+@echo   INSTALL_DIR             : %INSTALL_DIR%
+@echo   TEST_PACKAGE_DIR        : %TEST_PACKAGE_DIR%
+@echo   TEST_PACKAGE_BUILD_DIR  : %TEST_PACKAGE_BUILD_DIR%
 @echo ##########################################################################################################
 
 @REM step 1. checkout version
 @pushd %SOURCE_DIR%
 @if not "%errorlevel%" == "0" (
     @echo source dir not exists !!!
+    @echo need to run "git clone https://github.com/gflags/gflags.git gflags --branch v%VERSION%" first
     @goto end
 )
 
-git checkout release-%VERSION%
+git checkout v%VERSION%
+@if not "%errorlevel%" == "0" (
+    echo "checkout to branch v%VERSION% failed !!! (%cd%)"
+    goto end
+)
 git reset --hard
 
 @REM step 2. build library
 cmake -S %SOURCE_DIR% -B %BUILD_DIR% -G"Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX=%INSTALL_DIR% -DGFLAGS_BUILD_SHARED_LIBS=ON -DGFLAGS_BUILD_STATIC_LIBS=ON
 cmake --build %BUILD_DIR% --config Debug -j10
-cmake --build %BUILD_DIR% --config Release -j10
 cmake --install %BUILD_DIR% --config Debug
+cmake --build %BUILD_DIR% --config Release -j10
 cmake --install %BUILD_DIR% --config Release
+
+@REM step 3. test with test_package
+cmake -S %TEST_PACKAGE_DIR% -B %TEST_PACKAGE_BUILD_DIR% -G"Visual Studio 16 2019" -DCMAKE_PREFIX_PATH=%INSTALL_DIR%
+cmake --build %TEST_PACKAGE_BUILD_DIR% --config Debug -j10
+cmake --build %TEST_PACKAGE_BUILD_DIR% --config Release -j10
+%TEST_PACKAGE_BUILD_DIR%\Debug\test_package.exe
+@if not "%errorlevel%" == "0" (
+    @echo run test package with config Debug failed, abort!!!
+    @goto end
+)
+%TEST_PACKAGE_BUILD_DIR%\Release\test_package.exe
+@if not "%errorlevel%" == "0" (
+    @echo run test package with config Release failed, abort!!!
+    @goto end
+)
 
 :end
 @popd
